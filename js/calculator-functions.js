68f95cc8eb13477714c9a42183e008c1a6362c26
Richmond <130723287+richmondpangan@users.noreply.github.com>
2023-07-24 19:02:43 +0800

    Added a function to populate the textbox
    - Added a function that displays the value of a button that was clicked to the textbox field.
    - The function is limited only to buttons that has value of 0-9 as of now.

--- js/calculator-functions.js
+++ js/calculator-functions.js
@@ -29,4 +29,70 @@ function operate(operator, firstNum, secondNum) {
     }
 }
 
-console.log(operate("*", 5, 5));
+// New functions
+
+let zeroBtn = document.getElementById('number0');
+let oneBtn = document.getElementById('number1');
+let twoBtn = document.getElementById('number2');
+let threeBtn = document.getElementById('number3');
+let fourBtn = document.getElementById('number4');
+let fiveBtn = document.getElementById('number5');
+let sixBtn = document.getElementById('number6');
+let sevenBtn = document.getElementById('number7');
+let eightBtn = document.getElementById('number8');
+let nineBtn = document.getElementById('number9');
+let displayResult = document.getElementById('displayResult'); 
+
+function populateTextbox() {
+    zeroBtn.addEventListener('click', () => {
+        let btnValue = zeroBtn.innerHTML;
+        displayResult.value += btnValue;
+    });
+
+    oneBtn.addEventListener('click', () => {
+        let btnValue = oneBtn.innerHTML;
+        displayResult.value += btnValue;
+    });
+
+    twoBtn.addEventListener('click', () => {
+        let btnValue = twoBtn.innerHTML;
+        displayResult.value += btnValue;
+    });
+
+    threeBtn.addEventListener('click', () => {
+        let btnValue = threeBtn.innerHTML;
+        displayResult.value += btnValue;
+    });
+
+    fourBtn.addEventListener('click', () => {
+        let btnValue = fourBtn.innerHTML;
+        displayResult.value += btnValue;
+    });
+
+    fiveBtn.addEventListener('click', () => {
+        let btnValue = fiveBtn.innerHTML;
+        displayResult.value += btnValue;
+    });
+
+    sixBtn.addEventListener('click', () => {
+        let btnValue = sixBtn.innerHTML;
+        displayResult.value += btnValue;
+    });
+
+    sevenBtn.addEventListener('click', () => {
+        let btnValue = sevenBtn.innerHTML;
+        displayResult.value += btnValue;
+    });
+
+    eightBtn.addEventListener('click', () => {
+        let btnValue = eightBtn.innerHTML;
+        displayResult.value += btnValue;
+    });
+
+    nineBtn.addEventListener('click', () => {
+        let btnValue = nineBtn.innerHTML;
+        displayResult.value += btnValue;
+    });
+}
+
+populateTextbox();
