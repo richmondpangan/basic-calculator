088878670374995501ef3664dabdc1391e164a4f
Richmond <130723287+richmondpangan@users.noreply.github.com>
2023-07-28 18:35:09 +0800

    Utilized the math.js library
    - Removed the previously written functions.
    - Added a function that populates the textbox when buttons are clicked, except for the clear and equals button.
    - Added a function that calculates the user input by utilizing the math.js library.
    - Added a function that displays the answer to the textbox when the equals button is clicked.

--- js/calculator-functions.js
+++ js/calculator-functions.js
@@ -1,98 +1,22 @@
-function add(firstNum, secondNum) {
-    return firstNum + secondNum;
-}
+// Buttons
+let buttons = document.querySelectorAll('.buttons');
+let equalBtn = document.getElementById('equal-btn');
+let clearBtn = document.getElementById('clear-btn');
 
-function subtract(firstNum, secondNum) {
-    return firstNum - secondNum;
-}
-
-function multiply(firstNum, secondNum) {
-    return firstNum * secondNum;
-}
-
-function divide(firstNum, secondNum) {
-    return firstNum / secondNum;
-}
-
-function operate(operator, firstNum, secondNum) {
-    if (operator === "+") {
-        return add(firstNum, secondNum);
-    }
-    else if (operator === "-") {
-        return subtract(firstNum, secondNum);
-    }
-    else if (operator === "*") {
-        return multiply(firstNum, secondNum);
-    }
-    else if (operator === "/") {
-        return divide(firstNum, secondNum);
-    }
-}
-
-// New functions
-
-let zeroBtn = document.getElementById('number0');
-let oneBtn = document.getElementById('number1');
-let twoBtn = document.getElementById('number2');
-let threeBtn = document.getElementById('number3');
-let fourBtn = document.getElementById('number4');
-let fiveBtn = document.getElementById('number5');
-let sixBtn = document.getElementById('number6');
-let sevenBtn = document.getElementById('number7');
-let eightBtn = document.getElementById('number8');
-let nineBtn = document.getElementById('number9');
+// Textbox Display
 let displayResult = document.getElementById('displayResult'); 
 
-function populateTextbox() {
-    zeroBtn.addEventListener('click', () => {
-        let btnValue = zeroBtn.innerHTML;
-        displayResult.value += btnValue;
+buttons.forEach(function (btn) {
+    btn.addEventListener('click', () => {
+        displayResult.value += btn.innerHTML;   // Populates the textbox when button is clicked
+        equalBtn.addEventListener('click', () => {
+            let answer = math.evaluate(displayResult.value); // Math.js function that computes the user input
+            displayResult.value = answer;   // Displays the answer to the textbox when equals button is clicked
+        });
     });
+});
 
-    oneBtn.addEventListener('click', () => {
-        let btnValue = oneBtn.innerHTML;
-        displayResult.value += btnValue;
-    });
-
-    twoBtn.addEventListener('click', () => {
-        let btnValue = twoBtn.innerHTML;
-        displayResult.value += btnValue;
-    });
-
-    threeBtn.addEventListener('click', () => {
-        let btnValue = threeBtn.innerHTML;
-        displayResult.value += btnValue;
-    });
-
-    fourBtn.addEventListener('click', () => {
-        let btnValue = fourBtn.innerHTML;
-        displayResult.value += btnValue;
-    });
-
-    fiveBtn.addEventListener('click', () => {
-        let btnValue = fiveBtn.innerHTML;
-        displayResult.value += btnValue;
-    });
-
-    sixBtn.addEventListener('click', () => {
-        let btnValue = sixBtn.innerHTML;
-        displayResult.value += btnValue;
-    });
-
-    sevenBtn.addEventListener('click', () => {
-        let btnValue = sevenBtn.innerHTML;
-        displayResult.value += btnValue;
-    });
-
-    eightBtn.addEventListener('click', () => {
-        let btnValue = eightBtn.innerHTML;
-        displayResult.value += btnValue;
-    });
-
-    nineBtn.addEventListener('click', () => {
-        let btnValue = nineBtn.innerHTML;
-        displayResult.value += btnValue;
-    });
-}
-
-populateTextbox();
+// Clears the textbox when AC button is clicked
+clearBtn.addEventListener('click', () => {
+    displayResult.value = "";
+});
